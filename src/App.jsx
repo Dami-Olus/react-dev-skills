993186ed88827f1923bb9fb792c5fa31d0caccd8
Damola Olusakin <olusakinadedamola@gmail.com>
2023-07-17 16:25:11 -0400

    added skills dataset and passed as a props to skill list

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import './App.css'
 import NewSkillForm from './assets/components/NewSkillForm'
 import SkillList from './assets/components/SkillList'
 
+const skills = [
+  { name: "HTML", level: 5 },
+  { name: "CSS", level: 3 },
+  { name: "JavaScript", level: 4 },
+  { name: "Python", level: 2 },
+];
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>React Dev Skills</h1>
-      <SkillList />
+      <SkillList skills={skills} />
       <hr />
       <NewSkillForm />
     </>
